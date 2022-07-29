4cd0fe463a87989629baf1e79bf7bb742474220b
aubrey3 <aubrey@aubrey.lgbt>
2022-07-29 12:08:09 -0400

    Fixin things

--- pysub.py
+++ pysub.py
@@ -1,13 +1,14 @@
+from argparse import ArgumentParser
 from pathlib import Path
 import magic
 import errno
 
-def sub(path: Path, encoding):
+def sub(path: Path, old: str, new: str, encoding: str):
     print(f"substituting text file - {path}")
     try:
         with path.open("r+", encoding=encoding) as file:
             text = file.read()
-            new_text = text.replace("old", "new")
+            new_text = text.replace(old, new)
             file.seek(0)
             file.write(new_text)
             file.truncate()
@@ -19,21 +20,30 @@ def sub(path: Path, encoding):
         else:
             print(f"Failed to read file (error: {e}) - {path}")
 
-def sub_rec(path: Path):
+def sub_rec(path: Path, old: str, new: str):
     if path.is_dir():
         for subpath in path.iterdir():
-            sub_rec(subpath)
+            sub_rec(subpath, old, new)
     elif path.is_file():
         magic_text = magic.from_file(str(path.resolve()))
         if magic_text.startswith("ASCII"):
-            sub(path, "ASCII")
+            sub(path, old, new, "ASCII")
         elif magic_text.startswith("UTF-8"):
-            sub(path, "UTF-8")
+            sub(path, old, new, "UTF-8")
 
 def main():
-    path = Path("test")
-    if path.exists():
-        sub_rec(path)
+    parser = ArgumentParser(description="Substitute text within files")
+    parser.add_argument("-o", "--old", required=True, help="old text to be replaced")
+    parser.add_argument("-n", "--new", required=True, help="new text to use in replacement")
+    parser.add_argument("-i", "--input", default=".", help="input path to search for files")
+
+    args = parser.parse_args()
+    
+    input_path = Path(args.input)
+    if input_path.exists():
+        sub_rec(input_path, args.old, args.new)
+    else:
+        parser.error("input path does not exist")
 
 if __name__ == "__main__":
     main()
